f8a4d34c95847bf2cf870ced058204b05869feb5
sivani1507 <80464851+sivani1507@users.noreply.github.com>
2021-04-15 13:31:39 +0530

    Add files via upload

--- 3_Implementation/inc/proto.h
+++ 3_Implementation/inc/proto.h
@@ -1,1 +1,62 @@
-
+/**
+ * @file proto.h
+ * @author Sivani R
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef _PROTO_H
+#define _PROTO_H
+/**
+ * @brief proto file which includes all the functions
+ * 
+ */
+void additon();
+/**
+ * @brief performs addition operation
+ * 
+ */
+void subtraction();
+/**
+ * @brief performs subtraction operation
+ * 
+ */
+void multiplication();
+/**
+ * @brief performs multiplication operation
+ * 
+ */
+void division();
+/**
+ * @brief performs division operation
+ * 
+ */
+void modulus();
+/**
+ * @brief performs modulus operation gives remainder
+ * 
+ */
+void power();
+/**
+ * @brief finds the power of a number
+ * 
+ */
+void factorial();
+/**
+ * @brief determines factorial of a number
+ * 
+ */
+void drawn();
+/**
+ * @brief draws a right angled traingle with numbers
+ * 
+ */
+void drawa();
+/**
+ * @brief draws a pyramid with * character
+ * 
+ */
+# endif
